25f3f5f37bf8dad84c801f2875fd3986ab8f4607
narayana <nayakilaxminarayana99@gmail.com>
2019-06-10 19:27:21 +0530

    javascript_promise

---
+++ java_script.js
@@ -0,0 +1,54 @@
+/*let p = new Promise((resolve,reject) =>{
+    let a=1+1
+    if (a==2){
+        resolve("u r in")
+    }
+    else {
+        reject("poor in math")
+    }
+})
+p.then((message ) => {
+    console.log("so"+ message)
+})
+.catch((message) => {
+    console.log("so" + message)
+})*/
+
+/*ar momsPromise = new Promise(function(resolve, reject) {
+    momsSavings = 20000;
+    priceOfPhone = 10000;
+    if (momsSavings > priceOfPhone) {
+      resolve({
+        brand: "iphone",
+        model: "6s"
+      });
+    } else {
+      reject("We donot have enough savings. Let us save some more money.");
+    }
+  });
+  momsPromise.then(function(value) {
+    console.log("Hurray I got this phone as a gift ", JSON.stringify(value));
+  });
+  momsPromise.catch(function(reason) {
+    console.log("Mom coudn't buy me the phone because ", reason);
+  });
+ /* momsPromise.finally(function() {
+    console.log(
+      "Irrespecitve of whether my mom can buy me a phone or not, I still love her"
+    );
+  });*/
+  
+
+// Promise
+var promise1 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve('foo');
+    }, 300);
+  });
+  
+  promise1.then(function(value) {
+    console.log(value);
+    // expected output: "foo"
+  });
+  
+  console.log(promise1);
